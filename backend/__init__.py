6963c39f8fa247c6cc0239288d0916b1e251d114
VARUN15VS <varun15ssrs@gmail.com>
2025-06-24 23:53:08 +0530

    Add __init__.py

---
+++ backend/__init__.py
@@ -0,0 +1,15 @@
+# app/__init__.py
+from flask import Flask
+from backend.config import Config
+from backend.extension import db,jwt,mail
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    # Binding extensions to app
+    db.init_app(app)
+    jwt.init_app(app)
+    mail.init_app(mail)
+
+    return app
